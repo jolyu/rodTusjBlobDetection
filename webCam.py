dfd5b80e2682a7200754a76fffe1cfcb2e2878b8
SakariaHAli <46136295+SakariaHAli@users.noreply.github.com>
2020-01-23 10:56:24 +0100

    La til adaptive thresholding
    Usikker på om dette er bedre, må testes.

--- webCam.py
+++ webCam.py
@@ -5,6 +5,22 @@ import numpy as np
 redLower = (0,10,10)                                                #100,130,50 skal være for å filtrere rød farge
 redUpper = (80,255,255)                                             #200,200,130
 
+def adaptive_thresh(img,thresh,max,type):
+   
+    if(type == "bin"):
+        ret,th1 = cv2.threshold(img,thresh,max,cv2.THRESH_BINARY) 
+        return th1
+    elif(type == "mean"):
+        img_blur = cv2.medianBlur(img,5)
+        ret,th1 = cv2.threshold(img_blur,thresh,max,cv2.THRESH_BINARY)
+        th2 = cv2.adaptiveThreshold(th1,max,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,11,2)
+        return th2
+    else:
+        img_blur = cv2.meadianBlur(img,5)
+        ret,th1 = cv2.threshold(img_blur,thresh,max,cv2.THRESH_BINARY)
+        th3 = cv2.adaptiveThreshold(th1,max,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
+        return th3 
+
 if __name__ == "__main__":
     cv2.namedWindow("preview")                                      #åpner vindu
     vc = cv2.VideoCapture(0)                                        #tar er bilde med webCam
@@ -17,15 +33,26 @@ if __name__ == "__main__":
     cv2.imshow("preview", frame)                                    #viser frame
     detector = bd.blobDetector()                                    #første detection
 
-    while rval:                                                     #fortsetter å ta bilder så lenge det går, methink
-    
+
+    while rval:                                                    #fortsetter å ta bilder så lenge det går, methink
+        
         rval, frame = vc.read()                                     #tar nytt bilde
-        frame = cv2.resize(frame, (650,500))                        #reskalerer for vindu, fungerer ikke som planlagt (endre pixler)
+        frame = cv2.resize(frame, (650,500))                        #reskalerer for vindu, fungerer ikke som planlagt (endre pixler)                      
         mask = cv2.inRange(frame, redLower, redUpper)               #har ingen anelse hvordan mask opplegget fungerer, men det må til for å filterer bort alt fra bildet
         mask = cv2.erode(mask, None, iterations=0)                  #som ikke er rødt
         mask = cv2.dilate(mask, None, iterations=0) 
         frame = cv2.bitwise_and(frame,frame,mask = mask)            #dette gir et rart bilde
         cv2.imshow("34", frame)
+       
+        #Adaptive thresh forsøk
+        th2 = adaptive_thresh(frame,127,255,"mean")
+        th1 = adaptive_thresh(frame,127,255,"gauss")
+        th = adaptive_thresh(frame,127,255,"bin")
+        cv2.imshow("mean", th2)
+        cv2.imshow("gauss", th1)
+        cv2.imshow("bin", th)
+
+        #Adaptive thresh forsøk 
         (thres, frame) = cv2.threshold(frame, 190, 255, cv2.THRESH_BINARY)  #men med å gjøre dette får man svart-rødt bilde
         cv2.imshow("1", frame)
         frame = 255 -frame                                          #her inverteres svart rødt bilde (detector-funksjonen detekterer mørke objekt på lys bakgrunn, dette kan endres though)
